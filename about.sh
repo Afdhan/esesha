352434c38f8a09b9a41e93232bfc5153dc848d81
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-14 20:57:50 +0700

    F

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#         Premium Auto Script         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By M AFDHAN Dan NezaVPN           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "#                                      #"
+echo -e "# Allah                                 #"
+echo -e "# My Family                                     #"
+echo -e "# SGDC-TEAM                                    #"
+echo -e "#-----------------------------------------------#"
+echo -e "=================================================" | lolcat
