433a0adf373bf96add202642c33dfc4b7f2387da
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-15 06:51:35 +0700

    F

--- setup.sh
+++ setup.sh
@@ -10,17 +10,6 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://akses.endka.ga:81/aksesku | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/Endka22"
-rm -f setup.sh
-exit 0
-fi
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
