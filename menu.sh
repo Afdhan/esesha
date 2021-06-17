33611e64fc88011801fefaa631eb17431c596950
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-17 11:46:14 +0700

    F

--- menu.sh
+++ menu.sh
@@ -22,94 +22,91 @@ ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
 WKT=$(curl -s ipinfo.io/timezone )
 IPVPS=$(curl -s ipinfo.io/ip )
-	echo "--------------- Welcome To AutoScript Premium ---------------"
-	echo "--------------------- M AFDHAN - NezaVPN --------------------"
-	echo " "
+	echo -e "\e[32m══════════════════════════════════════════\e[m"
+	echo -e "                                M AFDHAN - NEZAVPN            " | lolcat
+	echo -e "\e[32m══════════════════════════════════════════\e[m"
 	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
 	tram=$( free -m | awk 'NR==2 {print $2}' )
 	swap=$( free -m | awk 'NR==4 {print $2}' )
 	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+	
+    echo -e "\e[32m══════════════════════════════════════════\e[m"
+	echo -e "\e[032;1mCPU Model           : \e[0m $cname"
+	echo -e "\e[032;1mNumber Of Cores     : \e[0m $cores"
+	echo -e "\e[032;1mCPU Frequency       : \e[0m $freq MHz"
+	echo -e "\e[032;1mAmount Of RAM       : \e[0m $tram MB"
+	echo -e "\e[032;1mAmount Of Swap      : \e[0m $swap MB"
+	echo -e "\e[032;1mSystem Uptime       : \e[0m $up"
+	echo -e "\e[032;1mISP Name            : \e[0m $ISP"
+	echo -e "\e[032;1mCity                : \e[0m $CITY"
+	echo -e "\e[032;1mTimeZone            : \e[0m $WKT"
+	echo -e "\e[033;1mIP Adress VPS       : \e[0m $IPVPS"
+	echo -e "\e[033;1mTanggal             : \e[0m $date"
+	echo -e "\e[32m══════════════════════════════════════════\e[m"
 
-	echo -e "\e[032;1mCPU Model:\e[0m $cname"
-	echo -e "\e[032;1mNumber Of Cores:\e[0m $cores"
-	echo -e "\e[032;1mCPU Frequency:\e[0m $freq MHz"
-	echo -e "\e[032;1mAmount Of RAM:\e[0m $tram MB"
-	echo -e "\e[032;1mAmount Of Swap:\e[0m $swap MB"
-	echo -e "\e[032;1mSystem Uptime:\e[0m $up"
-	echo -e "\e[032;1mISP Name:\e[0m $ISP"
-	echo -e "\e[032;1mCity:\e[0m $CITY"
-	echo -e "\e[032;1mTime:\e[0m $WKT"
-	echo -e "\e[033;1mIP-VPS:\e[0m $IPVPS"
-	date | lolcat
-	echo -e "-----------------------------------------------------"
+echo -e "\e[32m═════════════════════════════════\e[m"
 
-echo -e ""
+echo -e "                      MENU TUNNELING" | lolcat
 
-echo -e "TUNELLING MENU" | lolcat
+echo -e "\e[32m═════════════════════════════════\e[m"
 
-echo -e " 1.  Control Panel SSH & OVPN"
+echo -e " 1 ⸩  Panel SSH & OVPN         5 ⸩  Panel SSR & Shadowsocks"
 
-echo -e " 2.  Control Panel Wireguard"
+echo -e " 2 ⸩  Panel Wireguard          6 ⸩  Panel Vmess/V2ray"
 
-echo -e " 3.  Control Panel L2TP & PPTP"
+echo -e " 3 ⸩  Panel L2TP & PPTP        7 ⸩  Panel Vless"
 
-echo -e " 4.  Control Panel SSTP"
+echo -e " 4 ⸩  Panel SSTP               8 ⸩  Panel Trojan"
 
-echo -e " 5.  Control Panel SSR & Shadowsocks"
+echo -e "\e[32m═════════════════════════════════\e[m"
 
-echo -e " 6.  Control Panel Vmess/V2ray"
+echo -e "                      MENU SYSTEM" | lolcat
 
-echo -e " 7.  Control Panel Vless"
+echo -e "\e[32m═════════════════════════════════\e[m"
 
-echo -e " 8.  Control Panel Trojan"
+echo -e " 9 ⸩   Tambah Subdomain Host Untuk VPS"
 
-echo -e ""
+echo -e " 10 ⸩  Perbarui Sertifikat V2RAY"
 
-echo -e "TOOLS SERVER" | lolcat
+echo -e " 11 ⸩  Ubah Port All VPN"
 
-echo -e " 9.   Add Subdomain Host For VPS"
+echo -e " 12 ⸩  Autobackup Data VPS"
 
-echo -e " 10.  Renew Certificate V2RAY"
+echo -e " 13 ⸩  Backup Data VPS"
 
-echo -e " 11.  Change Port All Account"
+echo -e " 14 ⸩  Restore Data VPS"
 
-echo -e " 12.  Autobackup Data VPS"
+echo -e " 15 ⸩  Menu Webmin"
 
-echo -e " 13.  Backup Data VPS"
+echo -e " 16 ⸩  Limit Bandwith Speed Server"
 
-echo -e " 14.  Restore Data VPS"
+echo -e " 17 ⸩  Cek Penggunaan Ram VPS"
 
-echo -e " 15.  Webmin Menu"
+echo -e " 18 ⸩  Reboot VPS"
 
-echo -e " 16.  Limit Bandwith Speed Server"
+echo -e " 19 ⸩  Speedtest VPS"
 
-echo -e " 17.  Check Usage of VPS Ram"
+echo -e " 20 ⸩  Informasi Display System"
 
-echo -e " 18.  Reboot VPS"
+echo -e " 21 ⸩  Info Script Auto Install"
 
-echo -e " 19.  Speedtest VPS"
+echo -e " 22 ⸩  Restart All Service"
 
-echo -e " 20.  Information Display System"
+echo -e " 23 ⸩  Set Auto Reboot"
 
-echo -e " 21.  Info Script Auto Install"
-
-echo -e " 22.  Restart All Service"
-
-echo -e " 23.  Set Auto Reboot"
-
-echo -e " 24.  Set Multi Login Akun"
+echo -e " 24 ⸩  Set Multi Login SSH"
 
 echo -e "\e[32m══════════════════════════════════════════\e[m"
 
-echo -e " x.   Exit From Putty / JuiceSSH / Termux"
+echo -e "  x ⸩   Keluar Dari Putty/JuiceSSH/Termux"
 
 echo -e "\e[32m══════════════════════════════════════════\e[m"
 
 echo -e ""
 
-read -p "     Please Input Number  [1-22 or x] :  "  num
+read -p "     Masukkan Nomor  [1-22 / x] :  "  num
 
 echo -e ""
 
@@ -267,8 +264,12 @@ exit
 
 *)
 
-echo "Please enter an correct number"
+echo "Nomor Yang Anda Masukkan Salah!"
+sleep 0.5
+clear
+menu
 
 ;;
 
 esac
+
