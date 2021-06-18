2a1a38b88bac6eb729bc1ac488ffb940d8566cf7
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-18 21:33:31 +0700

    F

--- trial.sh
+++ trial.sh
@@ -7,6 +7,7 @@ IP=$(wget -qO- ipinfo.io/ip);
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
 MYIP=$(wget -qO- ipinfo.io/ip);
+domain=$(cat /etc/v2ray/domain)
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -14,11 +15,6 @@ ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | 
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
 Pass=1
-echo Ping Host
-echo Cek Hak Akses...
-sleep 0.5
-echo Permission Accepted
-clear
 sleep 0.5
 echo Membuat Akun: $Login
 sleep 0.5
@@ -33,12 +29,14 @@ echo -e "Berikut Informasi Trial SSH & OpenVPN"
 echo -e "===============================" | lolcat
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
+echo -e "Domain         : $domain"
 echo -e "===============================" | lolcat
 echo -e "Host           : $IP"
 echo -e "CITY           : $CITY"
 echo -e "ISP            : $ISP"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
+echo -e "Websocket      : 2082"
 echo -e "Stunnel        :$ssl"
 echo -e "Port Squid     :$sqd"
 echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
@@ -46,7 +44,12 @@ echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "badvpn         : 7100-7300"
 echo -e "===============================" | lolcat
-echo -e "Aktif Selama   : 1 Hari"
+echo -e "Payload Websocket :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "Setting SSH Websocket :"
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
+echo -e "===============================" | lolcat
+echo -e "Aktif Selama   : $masaaktif Hari"
 echo -e "Berakhir Pada  : $exp"
 echo -e "Mod By M AFDHAN & NezaVPN"
-
