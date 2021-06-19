462cde128a7c58da268470606500201559e705b9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-19 15:09:22 +0700

    F

--- add-ssr.sh
+++ add-ssr.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://afdhan.github.io/izin.sh | grep $MYIP )
+IZIN=$( curl http://afdhan.github.io/sc/izin.sh | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Akses Diizinkan...${NC}"
