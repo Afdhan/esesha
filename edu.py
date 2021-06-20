e45945caee1aaaa8e9952c4e2d8176f9c99d7e64
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-20 19:23:19 +0700

    F

--- edu.py
+++ edu.py
@@ -9,7 +9,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-RESPONSE = 'HTTP/1.1 101 Connected to server SGDC-VPN\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 WIBU-VPN\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
