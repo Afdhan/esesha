a5f6c5294259f998fcaf023e702df6dd7328ecf8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-20 14:39:59 +0700

    F

--- usernew.sh
+++ usernew.sh
@@ -52,12 +52,18 @@ echo -e "CITY           : $CITY"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
 echo -e "Stunnel        :$ssl"
+echo -e "WebSocket      : 2082
 echo -e "Port Squid     :$sqd"
 echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "badvpn         : 7100-7300"
 echo -e "===============================" | lolcat
+echo -e "Payload Websocket :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "Setting SSH Websocket :"
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
 echo -e "Aktif Selama   : $masaaktif Hari"
 echo -e "Berakhir Pada  : $exp"
 echo -e "Mod By M AFDHAN & NezaVPN"
