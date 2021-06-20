f5e0f61adbd1dfa11d92422c4fd077d05e9f5f89
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-20 10:18:40 +0700

    F

---
+++ edukasi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+# ==============================
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/edu-proxy "https://raw.githubusercontent.com/Days-Project/Autoscript/main/edu.py"
+chmod +x /usr/local/bin/edu-proxy
+
+# Installing Service
+cat > /etc/systemd/system/edu-proxy.service << END
+[Unit]
+Description=Python Edu Proxy By Endka
+Documentation=https://endka-stores.xyz
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2082
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edu-proxy
+systemctl restart edu-proxy
+
+clear
