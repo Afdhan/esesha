7501878845dc7b3f6963a1d1ee95a274b172753d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-03 19:25:59 +0700

    F

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -162,11 +162,11 @@ accept = 443
 connect = 127.0.0.1:109
 
 [dropbear]
-accept = 222
+accept = 990
 connect = 127.0.0.1:22
 
 [dropbear]
-accept = 777
+accept = 171
 connect = 127.0.0.1:22
 
 [openvpn]
