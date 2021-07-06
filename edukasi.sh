9fb8a704b9df0831341be6a953bdc41ddfa64236
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-06 12:10:15 +0700

    F

--- edukasi.sh
+++ edukasi.sh
@@ -6,8 +6,8 @@ chmod +x /usr/local/bin/edu-proxy
 # Installing Service
 cat > /etc/systemd/system/edu-proxy.service << END
 [Unit]
-Description=Python Edu Proxy By Endka
-Documentation=https://endka-stores.xyz
+Description=Python Edu Proxy
+Documentation=https://www.nezavpn.com
 After=network.target nss-lookup.target
 [Service]
 Type=simple
@@ -15,7 +15,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2082
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2086
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
@@ -25,4 +25,5 @@ systemctl daemon-reload
 systemctl enable edu-proxy
 systemctl restart edu-proxy
 
+rm -f edukasi.sh
 clear
