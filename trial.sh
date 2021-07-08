a205edff94ab74bc49fb2cba6f40751555f18406
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-08 15:13:27 +0700

    F

--- trial.sh
+++ trial.sh
@@ -3,6 +3,8 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
 MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
@@ -21,11 +23,8 @@ domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
-drop="$(cat ~/log-install.txt | grep -w "Dropbear" | cut -d: -f2)"
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
-ws="$(cat ~/log-install.txt | grep -w "WebSocket" | cut -d: -f2)"
-bad="$(cat ~/log-install.txt | grep -w "Badvpn" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
@@ -36,43 +35,34 @@ sleep 0.5
 echo Setting Password: $Pass
 sleep 0.5
 clear
-tnggl=$(date +"%d-%B-%Y")
-useradd -e `date -d "1 days" +"%Y-%m-%d"` -s /bin/false -M $Login
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "=================================" | lolcat
-echo -e "       Trial SSH & OpenVPN "
-echo -e "=================================" | lolcat
+echo -e "Berikut Informasi Trial SSH & OpenVPN"
+echo -e "===============================" | lolcat
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
 echo -e "Domain         : $domain"
-echo -e "=================================" | lolcat
-echo -e "Host/IP        : $MYIP"
+echo -e "===============================" | lolcat
+echo -e "Host           : $MYIP"
+echo -e "CITY           : $CITY"
+echo -e "ISP            : $ISP"
 echo -e "OpenSSH        : 22"
-echo -e "Dropbear       : $drop"
-echo -e "Stunnel        : $ssl"
-echo -e "WebSocket      : $ws"
-echo -e "Squid          : $sqd"
-echo -e "BadVPN         : $bad"
-echo -e "=================================" | lolcat
-echo -e "          Config OpenVPN"
-echo -e "=================================" | lolcat
-echo -e "TCP $ovpn      : http://$domain:81/client-tcp-$ovpn.ovpn"
-echo -e "UDP $ovpn2     : http://$domain:81/client-udp-$ovpn2.ovpn"
-echo -e "SSL 442        : http://$domain:81/client-tcp-SSL.ovpn"
-echo -e "ZIP FILE       : http://$domain:81/all-ovpn.zip"
-echo -e "=================================" | lolcat
-echo -e "        Setting WebSocket"
-echo -e "=================================" | lolcat
-echo -e "Setting Payload "
+echo -e "Dropbear       : 109, 143"
+echo -e "Stunnel        :$ssl"
+echo -e "Port Squid     :$sqd"
+echo -e "OpenVPN        : TCP $ovpn http://$MYIP:81/client-tcp-$ovpn.ovpn"
+echo -e "OpenVPN        : UDP $ovpn2 http://$MYIP:81/client-udp-$ovpn2.ovpn"
+echo -e "OpenVPN        : SSL 442 http://$MYIP:81/client-tcp-ssl.ovpn"
+echo -e "badvpn         : 7100-7300"
+echo -e "===============================" | lolcat
+echo -e "Payload Websocket :"
 echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e ""
-echo -e "Setting SSH "
-echo -e "bimbel.ruangguru.com:$ws@$Login:$Pass"
-echo -e "=================================" | lolcat
-echo -e "Aktif Selama  : 1 Hari"
-echo -e "Dibuat Pada   : $tnggl"
-echo -e "Berakhir Pada : $exp"
-echo -e "---------------------------------" | lolcat
-echo -e "- Mod By M AFDHAN & NezaVPN"
+echo -e "===============================" | lolcat
+echo -e "Setting SSH Websocket :"
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
+echo -e "===============================" | lolcat
+echo -e "Aktif Selama   : 1 Hari"
+echo -e "Berakhir Pada  : $exp"
+echo -e "Mod By M AFDHAN & NezaVPN"
