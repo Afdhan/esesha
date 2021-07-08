4e760a85d7f5d55290f8b00e2e14f30b4048ccf5
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-08 14:01:55 +0700

    F

--- ipsec.sh
+++ ipsec.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# Debian 9 & 10 64bit
-# Ubuntu 18.04 & 20.04 bit
-# Centos 7 & 8 64bit 
-# ==================================================
 
-VPN_IPSEC_PSK='myvpn'
+VPN_IPSEC_PSK='vpnsantuy'
 NET_IFACE=$(ip -o $NET_IFACE -4 route show to default | awk '{print $5}');
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 source /etc/os-release
@@ -16,16 +12,13 @@ bigecho "VPN setup in progress... Please be patient."
 # Create and change to working dir
 mkdir -p /opt/src
 cd /opt/src
-
-bigecho "Trying to auto discover IP of this server..."
 PUBLIC_IP=$(wget -qO- ipinfo.io/ip);
 
-bigecho "Installing packages required for the VPN..."
 if [[ ${OS} == "centos" ]]; then
 epel_url="https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm"
 yum -y install epel-release || yum -y install "$epel_url" 
 
-bigecho "Installing packages required for the VPN..."
+bigecho "Menginstall Paket VPN..."
 
 REPO1='--enablerepo=epel'
 REPO2='--enablerepo=*server-*optional*'
@@ -52,7 +45,7 @@ apt-get -y install libnss3-dev libnspr4-dev pkg-config \
   libcurl4-nss-dev flex bison gcc make libnss3-tools \
   libevent-dev ppp xl2tpd pptpd
 fi
-bigecho "Compiling and installing Libreswan..."
+bigecho "Menginstall Libreswan..."
 
 SWAN_VER=3.32
 swan_file="libreswan-$SWAN_VER.tar.gz"
@@ -95,7 +88,7 @@ if ! /usr/local/sbin/ipsec --version 2>/dev/null | grep -qF "$SWAN_VER"; then
   exiterr "Libreswan $SWAN_VER failed to build."
 fi
 
-bigecho "Creating VPN configuration..."
+bigecho "Membuat Konfigurasi VPN..."
 
 L2TP_NET=192.168.42.0/24
 L2TP_LOCAL=192.168.42.1
--- vpn.sh
+++ vpn.sh
@@ -97,7 +97,7 @@ cat /etc/openvpn/server/ca.crt >> /etc/openvpn/client-tcp-1194.ovpn
 echo '</ca>' >> /etc/openvpn/client-tcp-1194.ovpn
 
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 1194 )
-cp /etc/openvpn/client-tcp-1194.ovpn /home/vps/public_html/client-tcp-1194.ovpn
+cp /etc/openvpn/client-tcp-1194.ovpn /home/vps/public_html/OpenVPN-TCP-1194.ovpn
 
 # masukkan certificatenya ke dalam config client UDP 2200
 echo '<ca>' >> /etc/openvpn/client-udp-2200.ovpn
@@ -105,7 +105,7 @@ cat /etc/openvpn/server/ca.crt >> /etc/openvpn/client-udp-2200.ovpn
 echo '</ca>' >> /etc/openvpn/client-udp-2200.ovpn
 
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 2200 )
-cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/client-udp-2200.ovpn
+cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/OpenVPN-UDP-2200.ovpn
 
 # masukkan certificatenya ke dalam config client SSL
 echo '<ca>' >> /etc/openvpn/client-tcp-ssl.ovpn
@@ -113,8 +113,17 @@ cat /etc/openvpn/server/ca.crt >> /etc/openvpn/client-tcp-ssl.ovpn
 echo '</ca>' >> /etc/openvpn/client-tcp-ssl.ovpn
 
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( SSL )
-cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/client-tcp-ssl.ovpn
+cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/OpenVPN-TCP-SSL.ovpn
 
+mkdir /root/OVPN
+cp /etc/openvpn/client-tcp-ssl.ovpn OVPN
+cp /etc/openvpn/client-udp-2200.ovpn OVPN
+cp /etc/openvpn/client-tcp-1194.ovpn OVPN
+cd /root
+zip -r openvpn.zip OVPN > /dev/null 2>&1
+cp /root/openvpn.zip /home/vps/public_html/Configs-OpenVPN.zip
+rm -rf /root/OVPN
+rm -f /root/openvpn.zip
 #firewall untuk memperbolehkan akses UDP dan akses jalur TCP
 
 iptables -t nat -I POSTROUTING -s 10.6.0.0/24 -o $ANU -j MASQUERADE
