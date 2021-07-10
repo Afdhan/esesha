c9c1e8e2bdec63bdc5e5e19740940c0f86c1c9c8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-10 20:54:19 +0700

    F

--- about.sh
+++ about.sh
@@ -4,6 +4,8 @@ clear
 echo -e "=================================================" | lolcat
 echo -e "#     AutoScript VPS By M AFDHAN - NezaVPN      #"
 echo -e "#-----------------------------------------------#"
+echo -e "#             OPERATING SYSTEM                  #"
+echo -e "             -------------------                 "
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
@@ -18,4 +20,5 @@ echo -e "# Makhluk Bumi                                  #"
 echo -e "# Icipers Nusantara                             #"
 echo -e "# All Admin Dan All Member                      #"
 echo -e "#-----------------------------------------------#"
+echo -e "- Mod By M AFDHAN & NezaVPN"
 echo -e "=================================================" | lolcat
