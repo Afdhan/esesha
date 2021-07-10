53e8b3a3c1b7535a83f77dc1c70dea20e95a0219
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-10 21:04:49 +0700

    F

--- autokill.sh
+++ autokill.sh
@@ -3,8 +3,8 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[ON]${Font_color_suffix}"
-Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
+Info="${Green_font_prefix}[Enable]${Font_color_suffix}"
+Error="${Red_font_prefix}[Disable]${Font_color_suffix}"
 cek=$(grep -c -E "^# Autokill" /etc/cron.d/tendang)
 if [[ "$cek" = "1" ]]; then
 sts="${Info}"
@@ -15,13 +15,14 @@ clear
 echo -e ""
 echo -e "======================================" | lolcat
 echo -e ""
-echo -e "     Status Autokill $sts        "
+echo -e "  Status Auto Kill Saat Ini $sts        "
 echo -e ""
 echo -e "     [1]  Auto Kill 5 Minutes"
 echo -e "     [2]  Auto Kill 10 Minutes"
 echo -e "     [3]  Auto Kill 15 Minutes"
 echo -e "     [4]  Nonaktifkan AutoKill MultiLogin"
 echo -e "     [x]  Keluar"
+echo -e ""
 echo -e "======================================" | lolcat                                                                                                         
 echo -e ""
 read -p "     Pilih Nomor [1-4 / x] :  " AutoKill
