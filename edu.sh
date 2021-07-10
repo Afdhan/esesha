e008b9a6c1606beb4873c593b15932b3ca476ad3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-10 21:40:27 +0700

    F

--- edu.sh
+++ edu.sh
@@ -19,7 +19,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2082
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2086
 Restart=on-failure
 
 [Install]
