6396659b8190a8fa6f9f80f57ad3444b0bccf519
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-10 20:45:45 +0700

    F

--- host.sh
+++ host.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-echo "================================"
-read -rp "Masukkan Domain: " -e DOMAIN
+echo "==============================" | lolcat
 echo ""
-echo "Domain: ${DOMAIN}" 
+echo "Domain Bawaan Script: nezavpn.com" 
 echo ""
 read -rp "Masukkan Subdomain: " -e sub
-echo "Pointing Domain..."
+echo "Menganalisis Subomain..."
 sleep 0.5
 clear
+DOMAIN=nezavpn.com
 SUB_DOMAIN=${sub}.${DOMAIN}
 CF_ID=muhammad.neizam@gmail.com
 CF_KEY=10da726480584a601ecc58b12621f0b0102ec
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Pointing DNS Untuk Domain ${SUB_DOMAIN}..."
-sleep 0.5
+sleep 1
 clear
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
@@ -39,5 +39,6 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "Host : $SUB_DOMAIN"
+echo "Host/Domain Berhasil Disimpan"
+echo "Domain Anda : $SUB_DOMAIN"
 echo "IP=$SUB_DOMAIN" >> /var/lib/premium-script/ipvps.conf
