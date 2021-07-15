bdb66399738abdd50ea2704c755e99ec037f06ac
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-16 01:01:13 +0700

    F

--- wss.sh
+++ wss.sh
@@ -8,26 +8,29 @@ echo -e ""
 echo -e "======================================" | lolcat
 echo -e ""
 echo -e "     [1]  Buat Akun Vmess Websocket"
-echo -e "     [2]  Hapus Akun Vmess Websocket"
-echo -e "     [3]  Perbarui Akun Vmess"
-echo -e "     [4]  Cek Login Vmess"
+echo -e "     [2]  Buat Trial Vmess Websocket"
+echo -e "     [3]  Hapus Akun Vmess Websocket"
+echo -e "     [4]  Perbarui Akun Vmess"
+echo -e "     [5]  Cek Login Vmess"
 echo -e "     [x]  Keluar"
 echo -e ""
 echo -e "======================================" | lolcat
 echo -e ""
-read -p "     Pilih Nomor  [1-4 / x] :  " port
+read -p "     Pilih Nomor  [1-5 / x] :  " port
 echo -e ""
 case $port in
 1)
 add-ws
 ;;
 2)
+trialws
+3)
 del-ws
 ;;
-3)
+4)
 renew-ws
 ;;
-4)
+5)
 cek-ws
 ;;
 x)
