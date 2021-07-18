ddffc98300ed396da73a782f241ced33689c3f22
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-18 20:31:16 +0700

    F

--- renew-ssr.sh
+++ renew-ssr.sh
@@ -25,6 +25,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/shadowsocksr/akun.conf")
 		fi
 	done
 read -p "Expired (days): " masaaktif
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
 user=$(grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
@@ -32,14 +36,14 @@ d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
-exp4=`date -d "$exp3 days" +"%d-%B-%Y"`
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 tgl=$(echo "$exp4" | cut -d- -f3)
 bln=$(echo "$exp4" | cut -d- -f2)
 sed -i "s/### $user $exp/### $user $exp4/g" /usr/local/shadowsocksr/akun.conf
 clear
 echo ""
-echo " Akun Shadowsocks-R Berhasil Diperbarui"
+echo " Akun ShadowsocksR Berhasil Diperbarui"
 echo " ==========================" | lolcat
-echo " Client Name : $user"
-echo " Expired On  : $exp4"
+echo " Username      : $user"
+echo " Aktif Sampai  : $expe"
 echo " ==========================" | lolcat
