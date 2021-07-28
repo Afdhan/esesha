663ca0cb8bc54e0492ff13432efff915c62c6fce
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-29 00:21:25 +0700

    F

--- port-ssl.sh
+++ port-ssl.sh
@@ -26,7 +26,7 @@ if [[ -z $cek ]]; then
 sed -i "s/$ssl/$stl/g" /etc/stunnel/stunnel.conf
 sed -i "s/   - Stunnel4                : $ssl, $ssl2/   - Stunnel4                : $stl, $ssl2/g" /root/log-install.txt
 /etc/init.d/stunnel4 restart > /dev/null
-echo -e "\e[032;1mPort $stl modified successfully\e[0m"
+echo -e "\e[032;1mPort $stl Berhasil Diperbarui\e[0m"
 else
 echo "Port $stl Sudah Digunakan!"
 fi
@@ -42,7 +42,7 @@ if [[ -z $cek ]]; then
 sed -i "s/$ssl2/$stl/g" /etc/stunnel/stunnel.conf
 sed -i "s/   - Stunnel4                : $ssl, $ssl2/   - Stunnel4                : $ssl, $stl/g" /root/log-install.txt
 /etc/init.d/stunnel4 restart > /dev/null
-echo -e "\e[032;1mPort $stl modified successfully\e[0m"
+echo -e "\e[032;1mPort $stl Berhasil Diperbarui\e[0m"
 else
 echo "Port $stl Sudah Digunakan!"
 fi
