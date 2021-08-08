e5822f0a64cc71d6322a3fc90c6576e46b05afe6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-09 00:37:43 +0700

    F

--- updatee.sh
+++ updatee.sh
@@ -43,9 +43,9 @@ cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/UDP.ovpn
 cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/SSL.ovpn
 
 mkdir /root/OpenVPN
-cp -r /etc/openvpn/client-tcp-ssl.ovpn /OpenVPN/SSL.ovpn
-cp -r /etc/openvpn/client-udp-2200.ovpn /OpenVPN/UDP.ovpn
-cp -r /etc/openvpn/client-tcp-1194.ovpn /OpenVPN/TCP.ovpn
+cp -r /etc/openvpn/client-tcp-ssl.ovpn OpenVPN/SSL.ovpn
+cp -r /etc/openvpn/client-udp-2200.ovpn OpenVPN/UDP.ovpn
+cp -r /etc/openvpn/client-tcp-1194.ovpn OpenVPN/TCP.ovpn
 cd /root
 zip -r openvpn.zip OpenVPN > /dev/null 2>&1
 cp -r /root/openvpn.zip /home/vps/public_html/ALL.zip
