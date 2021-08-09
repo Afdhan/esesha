d04ae33e5e8834ec360d67f6ff5f5968c5a1a734
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-09 13:14:44 +0700

    F

--- info.sh
+++ info.sh
@@ -37,121 +37,121 @@ fail2ban=$(systemctl status fail2ban | grep -i "active (running)")
 
 #=================================================================================================
 
-if [ ! -e $wsdrop ]; then
+if [[ $wsdrop == "" ]]; then
       swsdrop=$ERROR
 else
       swsdrop=$AKTIF
 fi
 
-if [ ! -e $wstls ]; then
+if [[ $wstls == "" ]]; then
       swstls=$ERROR
 else
       swstls=$AKTIF
 fi
 
-if [ ! -e $wsopen ]; then
+if [[ $wsopen == "" ]]; then
       swsopen=$ERROR
 else
       swsopen=$AKTIF
 fi
 
-if [ ! -e $wsovpn ]; then
+if [[ $wsovpn == "" ]]; then
       swsovpn=$ERROR
 else
       swsovpn=$AKTIF
 fi
 
-if [ ! -e $v2ray ]; then
+if [[ $v2ray == "" ]]; then
       sv2ray=$ERROR
 else
       sv2ray=$AKTIF
 fi
 
-if [ ! -e $trojan ]; then
+if [[ $trojan = "" ]]; then
       strojan=$ERROR
 else
       strojan=$AKTIF
 fi
 
-if [ ! -e $ipsec ]; then
+if [[ $ipsec == "" ]]; then
       sipsec=$ERROR
 else
       sipsec=$AKTIF
 fi
 
-if [ ! -e $shadow ]; then
+if [[ $shadow == "" ]]; then
       sshadow=$ERROR
 else
       sshadow=$AKTIF
 fi
 
-if [ ! -e $ssr ]; then
+if [[ $ssr == "" ]]; then
       sssr=$ERROR
 else
       sssr=$AKTIF
 fi
 
-if [ ! -e $sstp ]; then
+if [[ $sstp == "" ]]; then
       ssstp=$ERROR
 else
       ssstp=$AKTIF
 fi
 
-if [ ! -e $l2tp ]; then
+if [[ $l2tp == "" ]]; then
       sl2tp=$ERROR
 else
       sl2tp=$AKTIF
 fi
 
-if [ ! -e $pptp ]; then
+if [[ $pptp == "" ]]; then
       spptp=$ERROR
 else
       spptp=$AKTIF
 fi
 
-if [ ! -e $ssh ]; then
+if [[ $ssh == "" ]]; then
       sssh=$ERROR
 else
       sssh=$AKTIF
 fi
 
-if [ ! -e $ssl ]; then
+if [[ $ssl == "" ]]; then
       sssl=$ERROR
 else
       sssl=$AKTIF
 fi
 
-if [ ! -e $drop ]; then
+if [[ $drop == "" ]]; then
       sdrop=$ERROR
 else
       sdrop=$AKTIF
 fi
 
-if [ ! -e $ovpn ]; then
+if [[ $ovpn == "" ]]; then
       sovpn=$ERROR
 else
       sovpn=$AKTIF
 fi
 
-if [ ! -e $nginx ]; then
+if [[ $nginx == "" ]]; then
       snginx=$ERROR
 else
       snginx=$AKTIF
 fi
 
-if [ ! -e $squid ]; then
+if [[ $squid == "" ]]; then
       ssquid=$ERROR
 else
       ssquid=$AKTIF
 fi
 
-if [ ! -e $cron ]; then
+if [[ $cron == "" ]]; then
       scron=$ERROR
 else
       scron=$AKTIF
 fi
 
-if [ ! -e $fail2ban ]; then
+if [[ $fail2ban == "" ]]; then
       sfail2ban=$ERROR
 else
       sfail2ban=$AKTIF
@@ -161,30 +161,30 @@ fi
 clear
 neofetch
 cat /root/log-install.txt
-echo -e "######################################################################" | lolcat
+echo -e "###################################################################################" | lolcat
 echo -e ""
-echo -e "${cyan}===================================-[ SERVICES STATUS ]-=============================${off}"
-echo -e "${green}"
-echo -e "   - Dropbear                                    : $sdrop "
-echo -e "   - OpenSSSH                                    : $sssh "
-echo -e "   - Stunnel4                                    : $sssl "
-echo -e "   - OpenVPN                                     : $sovpn "
-echo -e "   - V2ray                                       : $sv2ray "
-echo -e "   - Trojan                                      : $strojan "
-echo -e "   - IPSec                                       : $sipsec "
-echo -e "   - SSR                                         : $sssr "
-echo -e "   - Shadowsocks                                 : $sshadow "
-echo -e "   - SSTP                                        : $ssstp "
-echo -e "   - PPTP                                        : $spptp "
-echo -e "   - L2TP                                        : $sl2tp"
-echo -e "   - WebSocket Dropbear                          : $swsdrop"
-echo -e "   - WebSocket OpwnSSH                           : $swsopen"
-echo -e "   - WebSocket TLS                               : $swstls"
-echo -e "   - WebSocket OpenVPN                           : $swsovpn"
-echo -e "   - Nginx                                       : $snginx "
-echo -e "   - Squid                                       : $ssquid "
-echo -e "   - Cron                                        : $scron "
-echo -e "   - Fail2Ban                                    : $sfail2ban "
-echo -e "${off}"
-echo -e "${cyan}===================================-[ SERVICES STATUS ]-=============================${off}"
+echo -e "${cyan}==================================-[ SERVICES STATUS ]-============================${off}"
+echo -e ""
+echo -e "   - Dropbear                              : $sdrop "
+echo -e "   - OpenSSSH                              : $sssh "
+echo -e "   - Stunnel4                              : $sssl "
+echo -e "   - OpenVPN                               : $sovpn "
+echo -e "   - V2ray                                 : $sv2ray "
+echo -e "   - Trojan                                : $strojan "
+echo -e "   - IPSec                                 : $sipsec "
+echo -e "   - SSR                                   : $sssr "
+echo -e "   - Shadowsocks                           : $sshadow "
+echo -e "   - SSTP                                  : $ssstp "
+echo -e "   - PPTP                                  : $spptp "
+echo -e "   - L2TP                                  : $sl2tp"
+echo -e "   - WebSocket Dropbear                    : $swsdrop"
+echo -e "   - WebSocket OpwnSSH                     : $swsopen"
+echo -e "   - WebSocket TLS                         : $swstls"
+echo -e "   - WebSocket OpenVPN                     : $swsovpn"
+echo -e "   - Nginx                                 : $snginx "
+echo -e "   - Squid                                 : $ssquid "
+echo -e "   - Cron                                  : $scron "
+echo -e "   - Fail2Ban                              : $sfail2ban "
+echo -e ""
+echo -e "${cyan}==================================-[ SERVICES STATUS ]-============================${off}"
 echo -e ""
