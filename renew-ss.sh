d2c58dc852177c67b7943b06beaa0793e30ee9cf
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-09 07:08:17 +0700

    F

---
+++ renew-ss.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/shadowsocks-libev/akun.conf")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "User Tidak Ada!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Pilih Klien Yang Ingin Diperbarui"
+	echo " Klik CTRL+C untuk return"
+	echo -e "${cyan}===============================${off}"
+	grep -E "^### " "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
+	echo -e "${cyan}===============================${off}"
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Pilih Salah Satu [1]: " CLIENT_NUMBER
+		else
+			read -rp "Pilih Salah Satu [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+read -p "Expired (hari): " masaaktif
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+user=$(grep -E "^### " "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/shadowsocks-libev/akun.conf
+clear
+echo ""
+echo " Akun SS OBFS Berhasil Diperbarui"
+echo " ==========================" | lolcat
+echo " Username     : $user"
+echo " Aktif Sampai : $expe"
+echo " ==========================" | lolcat
