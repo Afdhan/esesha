9ece2ec8e96534b84a142c68994e37045c8bf7a2
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-09 07:18:26 +0700

    F

--- ltp.sh
+++ ltp.sh
@@ -10,7 +10,7 @@ off='\x1b[m'
 clear
 echo -e ""
 echo -e "${cyan}======================================${off}"
-echo -e "                 ${green}PANEL L2TP & PPTP${off}"
+echo -e "          ${green}PANEL L2TP & PPTP${off}"
 echo -e "${cyan}======================================${off}"
 echo -e "${green}"
 echo -e "     1 ⸩  Buat Akun L2TP"
