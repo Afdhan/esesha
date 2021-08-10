5d0fb3b835f5c96569463367aa562231c1991278
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-10 19:21:49 +0700

    F

--- add-host.sh
+++ add-host.sh
@@ -10,11 +10,12 @@ off='\x1b[m'
 read -rp "Masukkan Domain Anda : " -e host
 sleep 1
 pingg="ping $host"
-if [[ $pingg == "" ]]; then
-  echo -e "${red}Gagal! Domain Anda Tidak Aktif!${off}"
+#if [[ $pingg == "" ]]; then
+#  echo -e "${red}Gagal! Domain Anda Tidak Aktif!${off}"
   echo -e "${green}Pastikan Domain Anda Sudah DiPointing Sebelumnya!${off}"
-else
+#else
+sleep 1
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "Berhasil Menambahkan Domain VPS" | lolcat
 figlet -f slant AFDHAN - NEZA | lolcat
-fi
+#fi
