789c2f1a0bbc0f3d4ce1b8d6e89f29fa2afbcfe6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-17 00:42:11 +0700

    F

--- xp-ws.sh
+++ xp-ws.sh
@@ -10,7 +10,7 @@ do
 exp=$(grep -w "^### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$jam" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
+exp2=$(( (d1 - d2) / 7860 ))
 if [[ "$exp2" = "0" ]]; then
 sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/config.json
 sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/none.json
