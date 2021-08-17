bb83ba7ab338bdadb634da41a4713c6aad1e20c7
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-17 16:41:08 +0700

    F

--- trialws.sh
+++ trialws.sh
@@ -11,7 +11,6 @@ bold='\033[1m'
 off='\x1b[m'
 flag='\x1b[47;41m'
 
-#echo "10 * * * * xp-ws" >> /etc/crontab
 
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
@@ -21,17 +20,23 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
 if [ $MYIP = $IZIN ]; then
-clear
-echo -e "${green}Akses Diizinkan...${off}"
+echo -e "${green}Akses Diizinkan...${NC}"
 sleep 1
 else
 clear
-echo -e "${red}Akses Diblokir!${off}"
+echo -e "${red}Akses Diblokir!${NC}";
 echo "Hanya Untuk Pengguna Berbayar!"
 echo "Silahkan Hubungi Admin"
 exit 0
 fi
 clear
+sleep 0.5
+figlet -f slant E.R.R.O.R ! | lolcat
+sleep 2
+exit 0
+
+
+
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
@@ -40,19 +45,11 @@ domain=$IP
 fi
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		user=TRIAL_`</dev/urandom tr -dc X-Z0-9 | head -c2`
-		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
-
-		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-			clear
-			trialws
-		fi
-	done
+user=Trial-`</dev/urandom tr -dc X-Z0-9 | head -c2`
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (jam): " aktf
-sekar=$(TZ='Asia/Jakarta' date +%R)
-exp=`date -d "$aktf hour" +"%R"`
+tnggl=$(date +"%R")
+read -p "Expired (Jam): " ktf
+exp=`date -d "$ktf hour" +"%R"`
 sed -i '/#tls$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
 sed -i '/#none$/a\### '"$user $exp"'\
@@ -76,7 +73,7 @@ cat>/etc/v2ray/$user-none.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
-      "add": "IP_Bug_Anda",
+      "add": "IP_BUG",
       "port": "${none}",
       "id": "${uuid}",
       "aid": "2",
@@ -121,8 +118,6 @@ echo -e ""
 echo -e "${vmesslink2}" | lolcat
 echo -e "${cyan}=================================${off}"
 echo -e " ${green}Aktif Selama   : $aktf Jam"
-echo -e " Dibuat Pada    : $sekar WIB"
-echo -e " Berakhir Pada  : $exp WIB${off}"
 echo -e "${cyan}=================================${off}"
 echo -e " ${purple}- Mod By Dhansss X NezaVPN${off}"
 echo -e ""
