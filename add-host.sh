dc6005310e99aa26f174ac4659819fc2ec4dbb21
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-26 07:03:40 +0700

    F

--- add-host.sh
+++ add-host.sh
@@ -9,13 +9,112 @@ off='\x1b[m'
 
 read -rp "Masukkan Domain Anda : " -e host
 sleep 1
-pingg="ping $host"
-#if [[ $pingg == "" ]]; then
-#  echo -e "${red}Gagal! Domain Anda Tidak Aktif!${off}"
   echo -e "${green}Pastikan Domain Anda Sudah DiPointing Sebelumnya!${off}"
-#else
 sleep 1
+ptrg="$(cat ~/log-install.txt | grep -i TrojanGO | cut -d: -f2|sed 's/ //g')"
+uuid=$(cat /etc/trojan-go/uuid.txt)
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+cat <<EOF > /etc/trojan-go/config.json
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": $ptrg,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 81,
+    "log_level": 1,
+    "log_file": "/var/log/trojan-go.log",
+    "password": [
+        "$uuid"
+    ],
+  "disable_http_check": false,
+  "udp_timeout": 60,
+  "ssl": {
+    "verify": true,
+    "verify_hostname": true,
+    "cert": "/etc/v2ray/v2ray.crt",
+    "key": "/etc/v2ray/v2ray.key",
+    "key_password": "",
+    "cipher": "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+    "curves": "",
+    "prefer_server_cipher": true,
+    "sni": "$host",
+    "alpn": [
+      "http/1.1"
+    ],
+    "session_ticket": true,
+    "reuse_session": true,
+    "plain_http_response": "",
+    "fallback_addr": "127.0.0.1",
+    "fallback_port": $ptrg,
+    "fingerprint": ""
+  },
+  "tcp": {
+    "no_delay": true,
+    "keep_alive": true,
+    "prefer_ipv4": false
+  },
+  "mux": {
+    "enabled": true,
+    "concurrency": 64,
+    "idle_timeout": 60
+  },
+  "router": {
+    "enabled": true,
+    "bypass": [],
+    "proxy": [],
+    "block": [],
+    "default_policy": "proxy",
+    "domain_strategy": "as_is",
+    "geoip": "/etc/trojan-go/geoip.dat",
+    "geosite": "/etc/trojan-go/geosite.dat"
+  },
+  "websocket": {
+    "enabled": true,
+    "path": "/DhanZaa",
+    "host": "$host"
+  },
+  "shadowsocks": {
+    "enabled": false,
+    "method": "AES-128-GCM",
+    "password": ""
+  },
+  "transport_plugin": {
+    "enabled": false,
+    "type": "",
+    "command": "",
+    "plugin_option": "",
+    "arg": [],
+    "env": []
+  },
+  "forward_proxy": {
+    "enabled": false,
+    "proxy_addr": "",
+    "proxy_port": 0,
+    "username": "",
+    "password": ""
+  },
+  "mysql": {
+    "enabled": false,
+    "server_addr": "localhost",
+    "server_port": 3306,
+    "database": "",
+    "username": "",
+    "password": "",
+    "check_rate": 60
+  },
+  "api": {
+    "enabled": false,
+    "api_addr": "",
+    "api_port": 0,
+    "ssl": {
+      "enabled": false,
+      "key": "",
+      "cert": "",
+      "verify_client": false,
+      "client_cert": []
+    }
+  }
+}
+EOF
 echo "Berhasil Menambahkan Domain VPS" | lolcat
 figlet -f slant AFDHAN - NEZA | lolcat
-#fi
