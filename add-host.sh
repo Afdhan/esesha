456ed6f464524a75f17d2c2ec8b7ce971f79c646
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-28 12:13:47 +0700

    F

--- add-host.sh
+++ add-host.sh
@@ -116,5 +116,45 @@ cat <<EOF > /etc/trojan-go/config.json
   }
 }
 EOF
+
+cat > /etc/nginx/sites-enabled/wildcard_subdomain << END
+server {
+    server_name *.${host};
+    access_log /var/log/nginx/vps-access.log;
+    error_log /var/log/nginx/vps-error.log error;
+    root /home/vps/public_html;
+    
+    location / {
+       index  index.html index.htm index.php;
+       try_files $uri $uri/ /index.php?$args;
+    }
+    location ~\.php$ {
+        include /etc/nginx/fastcgi_params;
+        fastcgi_pass  127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
+    location ~ /\.ht {
+        deny all;
+    }
+    location ~* /(?:uploads|files)/.*\.php$ {
+        deny all;
+    }
+    location ~* \.(jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$ {
+         expires 356d;
+         add_header Cache-Control "public, no-transform";
+    }
+    
+    
+    location ~* \.(css|js)$ {
+         expires 2d;
+         add_header Cache-Control "public, no-transform";
+    }
+    listen: 81;
+}
+END
+
+ systemctl daemon-reload
+ systemctl restart nginx
 echo "Berhasil Menambahkan Domain VPS" | lolcat
 figlet -f slant AFDHAN - NEZA | lolcat
