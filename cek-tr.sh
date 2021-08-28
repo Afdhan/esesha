cfee5da637b6249e5d4f7c03f8dddf133c568bf7
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-28 22:02:54 +0700

    F

--- cek-tr.sh
+++ cek-tr.sh
@@ -2,13 +2,16 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+tr="$(cat ~/log-install.txt | grep -i TrojanGFW | cut -d: -f2|sed 's/ //g')"
+trg="$(cat ~/log-install.txt | grep -i TrojanGO | cut -d: -f2|sed 's/ //g')"
+
 data=( `cat /var/log/trojan.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
 echo -e "${red}============================${NC}";
 echo -e "   TROJAN-GFW USER LOGIN" | lolcat
 echo -e "${red}============================${NC}";
 for akun in "${data[@]}"
 do
-data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w 445 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
+data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w $tr | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
 echo -n > /tmp/iptrojan.txt
 for ip in "${data2[@]}"
 do
@@ -30,7 +33,7 @@ echo -e "    TROJAN-GO USER LOGIN" | lolcat
 echo -e "${red}============================${NC}";
 for akuun in "${datar[@]}"
 do
-datat=( `lsof -n | grep -i ESTABLISHED | grep trojan-go | awk '{print $9}' | cut -d':' -f2 | grep -w 443 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
+datat=( `lsof -n | grep -i ESTABLISHED | grep trojan-go | awk '{print $9}' | cut -d':' -f2 | grep -w $trg | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
 echo -n > /tmp/iptrojan-go.txt
 for ip in "${datat[@]}"
 do
