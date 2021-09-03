4d2511c27c7852af66d94573e8e9663208f98e7d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-09-03 17:39:20 +0700

    F

---
+++ add-ss.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+flag='\x1b[47;41m'
+
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+COUNTRY=$(curl -s ipinfo.io/country )
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${red}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+lastport1=$(grep "port_tls" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+lastport2=$(grep "port_http" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+if [[ $lastport1 == '' ]]; then
+tls=2443
+else
+tls="$((lastport1+1))"
+fi
+if [[ $lastport2 == '' ]]; then
+http=3443
+else
+http="$((lastport2+1))"
+fi
+echo ""
+#echo "Masukkan password"
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Password: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/shadowsocks-libev/akun.conf | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "Nama User Sudah Ada, Harap Masukkan Nama Lain!"
+			exit 1
+		fi
+	done
+read -p "Expired (hari): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+tgl2=$(date +"%d")
+bln2=$(date +"%b")
+thn2=$(date +"%Y")
+tnggl="$tgl2 $bln2, $thn2"
+cat > /etc/shadowsocks-libev/$user-tls.json<<END
+{   
+    "server":"0.0.0.0",
+    "server_port":$tls,
+    "password":"$user",
+    "timeout":60,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=tls"
+}
+END
+cat > /etc/shadowsocks-libev/$user-http.json <<-END
+{
+    "server":"0.0.0.0",
+    "server_port":$http,
+    "password":"$user",
+    "timeout":60,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=http"
+}
+END
+chmod +x /etc/shadowsocks-libev/$user-tls.json
+chmod +x /etc/shadowsocks-libev/$user-http.json
+
+systemctl start shadowsocks-libev-server@$user-tls.service
+systemctl enable shadowsocks-libev-server@$user-tls.service
+systemctl start shadowsocks-libev-server@$user-http.service
+systemctl enable shadowsocks-libev-server@$user-http.service
+tmp1=$(echo -n "aes-256-cfb:${user}@${domain}:$tls" | base64 -w0)
+tmp2=$(echo -n "aes-256-cfb:${user}@${domain}:$http" | base64 -w0)
+linkss1="ss://${tmp1}?plugin=obfs-local;obfs=tls;obfs-host=bug-anda.com"
+linkss2="ss://${tmp2}?plugin=obfs-local;obfs=http;obfs-host=bug-anda.com"
+echo -e "### $user $exp
+port_tls $tls
+port_http $http">>"/etc/shadowsocks-libev/akun.conf"
+service cron restart
+clear
+	echo -e ""
+	echo -e "${red}===================================${off}"
+        echo -e "${blue} ~> SHADOWSOCKS OBFS${off}"
+        echo -e "${red}===================================${off}"
+        echo -e " ISP            : $ISP"
+        echo -e " CITY           : $CITY"
+        echo -e " COUNTRY        : $COUNTRY"
+        echo -e " Server IP      : $MYIP"
+	echo -e " Server Host    : $domain"
+	echo -e " OBFS TLS       : $tls"
+	echo -e " OBFS HTTP      : $http"
+	echo -e " Password       : $user"
+	echo -e " Method         : aes-256-cfb"
+	echo -e "${red}===================================${off}"
+	echo -e "${blue}~> SS OBFS  TLS${off}"
+	echo -e ""
+        echo -e "$linkss1"
+	echo -e "${red}===================================${off}"
+	echo -e "${blue}~> SS OBFS HTTP${off}"
+	echo -e ""
+        echo -e "$linkss2"
+	echo -e "${red}===================================${off}"
+	echo -e " Aktif Selama   : $masaaktif Hari"
+echo -e " Dibuat Pada    : $tnggl"
+echo -e " Berakhir Pada  : $expe${off}"
+echo -e "${red}=================================${off}"
+echo -e " ${blue}- Mod By Dhansss X NezaVPN${off}"
+echo -e ""
