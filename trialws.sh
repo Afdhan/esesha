5ac1c196522d10fb61d46ae58f9ce33eb47a7c97
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-09-03 18:18:50 +0700

    F

---
+++ trialws.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+flag='\x1b[47;41m'
+
+
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+COUNTRY=$(curl -s ipinfo.io/country )
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Akses Diizinkan...${NC}"
+sleep 1
+else
+clear
+echo -e "${red}Akses Diblokir!${NC}";
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+user=TrialVmess`</dev/urandom tr -dc X-Z0-9 | head -c2`
+uuid=$(cat /proc/sys/kernel/random/uuid)
+tnggl=$(date +"%R")
+read -p "Expired (Jam): " ktf
+exp=`date -d "$ktf hour" +"%R"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/v2ray/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/DhanZaa",
+      "type": "none",
+      "host": "",
+      "tls": "tls"
+}
+EOF
+cat>/etc/v2ray/$user-none.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "IP_BUG_ANDA",
+      "port": "${none}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/DhanZaa",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/v2ray/$user-none.json)"
+systemctl restart v2ray
+systemctl restart v2ray@none
+service cron restart
+clear
+echo -e ""
+echo -e "${red}=================================${off}"
+echo -e "${blue} ~> TRIAL V2RAY VMESS${off}"
+echo -e "${red}=================================${off}"
+echo -e " ISP            : ${ISP}"
+echo -e " CITY           : ${CITY}"
+echo -e " COUNTRY        : ${COUNTRY}"
+echo -e " Remarks        : ${user}"
+echo -e " Domain         : ${domain}"
+echo -e " Port TLS       : ${tls}"
+echo -e " Port NON-TLS   : ${none}"
+echo -e " ID             : ${uuid}"
+echo -e " AlterID        : 2"
+echo -e " Security       : auto"
+echo -e " Network        : ws"
+echo -e " Path           : /DhanZaa"
+echo -e "${red}=================================${off}"
+echo -e "${blue}~> VMESS TLS${off}"
+echo -e ""
+echo -e "${vmesslink1}"
+echo -e "${red}=================================${off}"
+echo -e "${blue}~> VMESS NON-TLS${off}"
+echo -e ""
+echo -e "${vmesslink2}"
+echo -e "${red}=================================${off}"
+echo -e " Aktif Selama   : $masaaktif Jam"
+echo -e "${red}=================================${off}"
+echo -e " ${blue}- Mod By Dhansss X NezaVPN${off}"
+echo -e ""
