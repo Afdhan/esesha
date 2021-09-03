a1e793aadfc3748844fcf9986874b7a7ace1e17d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-09-03 18:05:31 +0700

    F

---
+++ add-sstp.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+red='\e[1;31m'
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+flag='\x1b[47;41m'
+
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+COUNTRY=$(curl -s ipinfo.io/country )
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${red}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username: " -e user
+		CLIENT_EXISTS=$(grep -w $user /var/lib/premium-script/data-user-sstp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "Nama User Sudah Ada, Harap Masukkan Nama Lain!"
+                        exit 1
+		fi
+	done
+read -p "Password: " pass
+read -p "Expired (hari): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+tgl2=$(date +"%d")
+bln2=$(date +"%b")
+thn2=$(date +"%Y")
+tnggl="$tgl2 $bln2, $thn2"
+cat >> /home/sstp/sstp_account <<EOF
+$user * $pass *
+EOF
+echo -e "### $user $exp">>"/var/lib/premium-script/data-user-sstp"
+clear
+echo -e ""
+echo -e "${red}================================${off}"
+echo -e "${blue} ~> SSTP VPN${off}"
+echo -e "${red}================================${off}"
+echo -e " ISP            : $ISP"
+echo -e " CITY           : $CITY"
+echo -e " COUNTRY        : $COUNTRY"
+echo -e " Server IP      : $MYIP"
+echo -e " Server Host    : $domain"
+echo -e " Port           : $sstp"
+echo -e " Username       : $user"
+echo -e " Password       : $pass"
+echo -e " Cert           : http://$domain:81/server.crt"
+echo -e "${red}================================${off}"
+echo -e " Aktif Selama   : $masaaktif Hari"
+echo -e " Dibuat Pada    : $tnggl"
+echo -e " Berakhir Pada  : $expe${off}"
+echo -e "${red}=================================${off}"
+echo -e " ${blue}- Mod By Dhansss X NezaVPN${off}"
+echo -e ""
